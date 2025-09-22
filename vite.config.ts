c1ff703bc02cea67d3277b19589d77ed9dbab070
PacificBathroom <chloejaysheppard@gmail.com>
2025-09-22 17:27:57 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: { outDir: "dist", sourcemap: false, target: "es2020" },
-  // (Optional) local dev proxy; Netlify handles /api in production via redirects
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8888",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+  plugins: [react()]
+})
