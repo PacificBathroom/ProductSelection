74000872f3ee4f1950f29135427a311e0b3cb72d
PacificBathroom <chloejaysheppard@gmail.com>
2025-10-03 08:17:13 +0000

    UI: serve styles from public/styles.css via index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css"; // <-- keep this line exactly (lowercase 'styles.css')
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
